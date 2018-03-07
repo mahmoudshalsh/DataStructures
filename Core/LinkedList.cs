7370f0616d8686ce176afeb17b3ced2bc0d14aa6
Mahmoud Shalsh <mahmoud.adel.shalsh@gmail.com>
2018-03-07 22:11:05 +0300

    ICollection Implementation for Linked List

--- Core/LinkedList.cs
+++ Core/LinkedList.cs
@@ -4,14 +4,16 @@ using System.Collections.Generic;
 
 namespace Core
 {
-    public class LinkedList<T> : IEnumerable<T>
+    public class LinkedList<T> : ICollection<T>
     {
         public LinkedListNode<T> Head { get; private set; } = null;
         public LinkedListNode<T> Tail { get; private set; } = null;
         public int Count { get; private set; } = 0;
+        public bool IsReadOnly { get; private set; } = false;
 
         #region Add
-        public void AddElementInFirst(LinkedListNode<T> item)
+
+        public void AddNodeInFirst(LinkedListNode<T> item)
         {
             if (IsEmpty())
                 Head = Tail = item;
@@ -23,9 +25,9 @@ namespace Core
 
             Count++;
         }
-        public void AddFirst(T item) => AddElementInFirst(new LinkedListNode<T>(item));
+        public void AddNodeInFirst(T item) => AddNodeInFirst(new LinkedListNode<T>(item));
 
-        public void AddElementInLast(LinkedListNode<T> item)
+        public void AddNodeInLast(LinkedListNode<T> item)
         {
             if (IsEmpty())
                 Head = Tail = item;
@@ -37,10 +39,12 @@ namespace Core
 
             Count++;
         }
-        public void AddLast(T item) => AddElementInLast(new LinkedListNode<T>(item));
+        public void AddNodeInLast(T item) => AddNodeInLast(new LinkedListNode<T>(item));
+
         #endregion
 
         #region Remove
+
         public void RemoveFirstNode()
         {
             if (!IsEmpty())
@@ -52,7 +56,6 @@ namespace Core
                 Count--;
             }
         }
-
         public void RemoveLastNode()
         {
             if (IsEmpty())
@@ -65,16 +68,17 @@ namespace Core
                 {
                     var iterator = Head;
                     while (iterator.Next != Tail)
-                    {
                         iterator = iterator.Next;
-                    }
                     Tail = iterator;
                     iterator.Next = null;
                 }
                 Count--;
             }
         }
+
         #endregion
+
+        #region ICollection
 
         public IEnumerator<T> GetEnumerator()
         {
@@ -85,6 +89,70 @@ namespace Core
                 iterator = iterator.Next;
             }
         }
+
+        public void Add(T item) => AddNodeInLast(item);
+
+        public void Clear()
+        {
+            Head = Tail = null;
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            var interval = Head;
+            while (interval != null)
+            {
+                if (item.Equals(interval.Value))
+                    return true;
+                interval = interval.Next;
+            }
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            var interval = Head;
+            int listIndex = 0;
+            int arrIndex = 0;
+            while (interval != null && arrIndex < array.Length && listIndex < Count)
+            {
+                if (arrayIndex == listIndex)
+                {
+                    while (interval != null)
+                    {
+                        array[arrIndex] = interval.Value;
+                        interval = interval.Next;
+                        arrIndex++;
+                        listIndex++;
+                    }
+                }
+                interval = interval != null ? interval.Next : null;
+                listIndex++;
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            var result = false;
+            if (Contains(item))
+            {
+                var interval = Head;
+                while (interval != null)
+                {
+                    if (interval.Next != null && item.Equals(interval.Next.Value))
+                    {
+                        interval.Next = interval.Next.Next;
+                        result = true;
+                        Count--;
+                    }
+                    interval = interval.Next;
+                }
+            }
+            return result;
+        }
+
+        #endregion
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
--- Tests/LinkedListShould.cs
+++ Tests/LinkedListShould.cs
@@ -4,21 +4,26 @@ using Xunit;
 
 namespace Tests
 {
-    public class LinkedListFeature
+    public class LinkedListShould
     {
+        LinkedList<int> linkedList;
+        LinkedListNode<int> tailExpected, middleExpected, headExpected;
+
+        public LinkedListShould()
+        {
+            linkedList = new LinkedList<int>();
+            tailExpected = new LinkedListNode<int>(5);
+            middleExpected = new LinkedListNode<int>(4, tailExpected);
+            headExpected = new LinkedListNode<int>(3, middleExpected);
+        }
+
         [Fact]
         public void AddFirst_CheckHead_CheckTail_CheckCount()
         {
-            // Arrange
-            LinkedList<int> linkedList = new LinkedList<int>();
-            LinkedListNode<int> tailExpected = new LinkedListNode<int>(5);
-            LinkedListNode<int> middleExpected = new LinkedListNode<int>(4, tailExpected);
-            LinkedListNode<int> headExpected = new LinkedListNode<int>(3, middleExpected);
-
             // Act
-            linkedList.AddElementInFirst(new LinkedListNode<int>(5));
-            linkedList.AddElementInFirst(new LinkedListNode<int>(4));
-            linkedList.AddElementInFirst(new LinkedListNode<int>(3));
+            linkedList.AddNodeInFirst(new LinkedListNode<int>(5));
+            linkedList.AddNodeInFirst(new LinkedListNode<int>(4));
+            linkedList.AddNodeInFirst(new LinkedListNode<int>(3));
 
             // Assert
             Assert.Equal(tailExpected, linkedList.Tail);
@@ -30,16 +35,10 @@ namespace Tests
         [Fact]
         public void AddLast_CheckHead_CheckTail_CheckCount()
         {
-            // Arrange
-            LinkedList<int> linkedList = new LinkedList<int>();
-            LinkedListNode<int> tailExpected = new LinkedListNode<int>(5);
-            LinkedListNode<int> middleExpected = new LinkedListNode<int>(4, tailExpected);
-            LinkedListNode<int> headExpected = new LinkedListNode<int>(3, middleExpected);
-
             // Act
-            linkedList.AddElementInLast(new LinkedListNode<int>(3));
-            linkedList.AddElementInLast(new LinkedListNode<int>(4));
-            linkedList.AddElementInLast(new LinkedListNode<int>(5));
+            linkedList.AddNodeInLast(new LinkedListNode<int>(3));
+            linkedList.AddNodeInLast(new LinkedListNode<int>(4));
+            linkedList.AddNodeInLast(new LinkedListNode<int>(5));
 
             // Assert
             Assert.Equal(tailExpected, linkedList.Tail);
@@ -49,15 +48,64 @@ namespace Tests
         }
 
         [Fact]
+        public void RemoveFirst_CheckHead_CheckTail_CheckCount()
+        {
+            // Arrange
+            linkedList.AddNodeInFirst(new LinkedListNode<int>(5));
+            linkedList.AddNodeInFirst(new LinkedListNode<int>(4));
+            linkedList.AddNodeInFirst(new LinkedListNode<int>(3));
+
+            // Act & Assert
+            linkedList.RemoveFirstNode();
+            Assert.Equal(middleExpected, linkedList.Head);
+            Assert.Equal(tailExpected, linkedList.Tail);
+            Assert.Equal(2, linkedList.Count);
+
+            linkedList.RemoveFirstNode();
+            Assert.Equal(tailExpected, linkedList.Head);
+            Assert.Equal(tailExpected, linkedList.Tail);
+            Assert.Equal(1, linkedList.Count);
+
+            linkedList.RemoveFirstNode();
+            Assert.Equal(null, linkedList.Head);
+            Assert.Equal(null, linkedList.Tail);
+            Assert.Equal(0, linkedList.Count);
+        }
+
+        [Fact]
+        public void RemoveLast_CheckHead_CheckTail_CheckCount()
+        {
+            // Arrange
+            linkedList.AddNodeInFirst(new LinkedListNode<int>(5));
+            linkedList.AddNodeInFirst(new LinkedListNode<int>(4));
+            linkedList.AddNodeInFirst(new LinkedListNode<int>(3));
+
+            // Act & Assert
+            linkedList.RemoveLastNode();
+            Assert.Equal(headExpected, linkedList.Head);
+            Assert.Equal(middleExpected, linkedList.Tail);
+            Assert.Equal(2, linkedList.Count);
+
+            linkedList.RemoveLastNode();
+            Assert.Equal(headExpected, linkedList.Head);
+            Assert.Equal(headExpected, linkedList.Tail);
+            Assert.Equal(1, linkedList.Count);
+
+            linkedList.RemoveLastNode();
+            Assert.Equal(null, linkedList.Head);
+            Assert.Equal(null, linkedList.Tail);
+            Assert.Equal(0, linkedList.Count);
+        }
+
+        [Fact]
         public void ReturnEnumerator_CheckHead_CheckTail_CheckCount()
         {
             // Arrange
-            LinkedList<int> linkedList = new LinkedList<int>();
-            linkedList.AddElementInFirst(new LinkedListNode<int>(5));
-            linkedList.AddElementInFirst(new LinkedListNode<int>(4));
-            linkedList.AddElementInFirst(new LinkedListNode<int>(3));
-            linkedList.AddElementInFirst(new LinkedListNode<int>(2));
-            linkedList.AddElementInFirst(new LinkedListNode<int>(1));
+            linkedList.AddNodeInFirst(new LinkedListNode<int>(5));
+            linkedList.AddNodeInFirst(new LinkedListNode<int>(4));
+            linkedList.AddNodeInFirst(new LinkedListNode<int>(3));
+            linkedList.AddNodeInFirst(new LinkedListNode<int>(2));
+            linkedList.AddNodeInFirst(new LinkedListNode<int>(1));
 
             // Act
             System.Collections.Generic.IEnumerable<int> actual = linkedList;
@@ -71,6 +119,90 @@ namespace Tests
             Assert.Equal(new LinkedListNode<int>(1), linkedList.Head);
             Assert.Equal(new LinkedListNode<int>(5), linkedList.Tail);
             Assert.Equal(5, linkedList.Count);
+        }
+
+        [Fact]
+        public void Add_CheckHead_CheckTail_CheckCount()
+        {
+            // Act
+            linkedList.Add(3);
+            linkedList.Add(4);
+            linkedList.Add(5);
+
+            // Assert
+            Assert.Equal(tailExpected, linkedList.Tail);
+            Assert.Equal(headExpected, linkedList.Head);
+            Assert.Equal(middleExpected, linkedList.Head.Next);
+            Assert.Equal(3, linkedList.Count);
+        }
+
+        [Fact]
+        public void Clear_CheckHead_CheckTail_CheckCount()
+        {
+            // Arrange
+            linkedList.Add(3);
+            linkedList.Add(4);
+            linkedList.Add(5);
+
+            // Act
+            linkedList.Clear();
+
+            // Assert
+            Assert.Equal(null, linkedList.Tail);
+            Assert.Equal(null, linkedList.Head);
+            Assert.Equal(0, linkedList.Count);
+        }
+
+        [Fact]
+        public void Contains_CheckHead_CheckTail_CheckCount()
+        {
+            // Arrange
+            linkedList.Add(3);
+            linkedList.Add(4);
+            linkedList.Add(5);
+
+            // Act
+            var actual1 = linkedList.Contains(4);
+            var actual2 = linkedList.Contains(1);
+
+            // Assert
+            Assert.Equal(true, actual1);
+            Assert.Equal(false, actual2);
+        }
+
+        [Fact]
+        public void CopyTo_CheckHead_CheckTail_CheckCount()
+        {
+            // Arrange
+            linkedList.Add(3);
+            linkedList.Add(4);
+            linkedList.Add(5);
+            int[] actual = new int[2];
+            int[] expected = new int[2] { 4, 5 };
+
+            // Act
+            linkedList.CopyTo(actual, 1);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Remove_CheckHead_CheckTail_CheckCount()
+        {
+            // Arrange
+            linkedList.Add(3);
+            linkedList.Add(4);
+            linkedList.Add(5);
+
+            // Act
+            linkedList.Remove(4);
+
+            // Assert
+            Assert.Equal(headExpected, linkedList.Head);
+            Assert.Equal(tailExpected, linkedList.Head.Next);
+            Assert.Equal(tailExpected, linkedList.Tail);
+            Assert.Equal(2, linkedList.Count);
         }
     }
 }
